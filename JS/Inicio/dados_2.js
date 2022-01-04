3093a58a78b1b27abd8ae2748a058226c5dc392c
Ana Oliveira <anaestefanodeoliveira@outlook.com>
2022-01-04 18:36:57 +0000

    update

---
+++ JS/Inicio/dados_2.js
@@ -0,0 +1,12 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco *(1 - desconto);
+
+console.log(precoComDesconto);
+
+let nome = 'Caderno'; // String (texto) -> Sequência de símbolos
+let categoria = 'Papelaria';
+console.log("Prdoduto:  " + nome + 
+            ' Categoria: , ' + categoria 
+            + ", Preço: " + preco
+            + ", Desconto: " + desconto);
---
+++ JS/Inicio/dados_3.js
@@ -0,0 +1,18 @@
+// Dados typeof number/ float 
+
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+
+// typeof Boolean -> true/false 
+let estaChovendo = true; // or false 
+console.log(typeof estaChovendo);
+
+//typeof String -> texto 
+console.log("Isto é uma String, tipo texto");  
+
+
