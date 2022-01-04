2298106c37eb614b997e9e213016c18b02241955
Ana Oliveira <anaestefanodeoliveira@outlook.com>
2022-01-04 17:19:35 +0000

    Update

---
+++ JS/Inicio/dados.js
@@ -0,0 +1,19 @@
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var final = preco+imposto;
+
+
+console.log(nome);
+console.log(quantidade); // valor literal = valor inteiro
+console.log(preco); 
+console.log(imposto);
+console.log(final); // junção de preço e imposto 
+
+nome = " Caneta Bic"; 
+console.log(nome);
+
+
+// var - pode ser modificado 
+// let - não pode ser modificado 
