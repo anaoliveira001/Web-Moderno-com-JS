106ef1cbf13cce65342f274164f5f3867813111a
Ana Oliveira <anaestefanodeoliveira@outlook.com>
2022-01-04 16:29:05 +0000

    atualização

--- Inicio/comentario.js
+++ Inicio/comentario.js
@@ -1,4 +1,14 @@
 // console.log é usado para exibir algo na tela do computador
 console.log("Passo #01");
-console.log("Passo #02");
+console.log("Passo #02"); // mais um exemplo de console.log
 console.log("Passo #03");
+
+/*
+Comentário em bloco
+permite que você quebre o
+comentário do seu código
+*/
+console.log('Comentário em Bloco');
+
+// sempre usar comentários relevantes em seu código para que
+// ele não fique poluído
--- Inicio/sentenca_1.js
+++ Inicio/sentenca_1.js
@@ -3,3 +3,6 @@ console.log("Legal 2");
 console.log('Bom Dia');
 console.log('Boa Tarde');
 console.log('Boa noite');
+
+//extenções:
+//Code Run; 
