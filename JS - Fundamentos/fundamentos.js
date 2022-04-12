672c9be0267df0a71bd7b18306538e7f82ca968a
Ana Paula Oliveira <anaestefanodeoliveira@outlook.com>
2022-04-12 15:20:20 +0100

    update

---
+++ JS - Fundamentos/fundamentos.js
@@ -0,0 +1,7 @@
+let a = 10; // variavel 1
+let b = 20; // variavel 2
+let soma = a+b; // soma = variavel 1 + variavel 2
+console.log(soma); // print - resultado da soma.
+
+
+const lasanha = [Queijo, Fiambre, Massa];
